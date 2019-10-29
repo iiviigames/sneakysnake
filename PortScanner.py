78946aa69d882b0daf9d3f648779653546dc56d0
iivii <iiviigames@pm.me>
2019-10-29 18:59:06 -0400

    :dragon:
    Updates

--- PortScanner.py
+++ PortScanner.py
@@ -1,23 +1,54 @@
 #!/usr/bin/python3
 
-#   Using hackthissite.org
+#	Using hackthissite.org
 
-#       IMPORTS         #
+#		IMPORTS			#
 #-----------------------#
 import socket
 
-#       GLOBALS         #
+#		GLOBALS			#
 #-----------------------#
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.settimeout(5)
+
 host = "137.74.187.103"
+localhost = "192.168.1.1"
 port = 443
+localport = 22
 
-#       FUNCTIONS       #
+#		FUNCTIONS		#
 #-----------------------#
-def port_scanner(port):
-    if s.connect_ex((host, port)):
-        print("The port is closed")
-    else:
-        print("The port is open")
 
-port_scanner(port)
+
+def port_scanner(port):
+	"""
+	Finds out if the port on a given host is open.
+	"""
+	if s.connect_ex((host, port)):
+		print("The port is closed")
+	else:
+		print("The port is open")
+
+
+def get_ip():
+	"""
+	Requests an IP Address and a Port Number from the user.
+	"""
+	host = input("Please enter the IP you wish to scan\n::: ")
+	port = str(input("Please enter the port number you wish to use\n::: "))
+	return host, port
+
+
+def connect_to(ip, port):
+	s.connect((ip, int(port)))
+
+
+def log_connection(amt):
+	print(s.recv(1024))
+
+#		MAINLOOP		#
+#-----------------------#
+
+host, port = get_ip()
+# print(host, port)
+#port_scanner(port)
